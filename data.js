c52dc02b022f6369227ebf465d808c15a2aaf595
Dmytro Bykoder <amidston@gmail.com>
2021-08-28 15:07:21 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,74 @@
+let songs = [
+    {
+        name: 'Rap God',
+        path: 'assets/Music/Eminem - Rap God.mp3',
+        artist: 'Eminem',
+        cover: 'assets/Images/cover 1.jpg'
+    },
+    {
+        name: '1000-7',
+        path: 'assets/Music/fem.love - 1000-7.mp3',
+        artist: 'fem.love',
+        cover: 'assets/Images/cover 2.jpg'
+    },
+    {
+        name: 'Feel Good Inc.',
+        path: 'assets/Music/Gorillaz - Feel Good Inc..mp3',
+        artist: 'Gorillaz',
+        cover: 'assets/Images/cover 3.jpg'
+    },
+    {
+        name: 'El Problema',
+        path: 'assets/Music/MORGENSHTERN feat. Тимати - El Problema.mp3',
+        artist: 'MORGENSHTERN & Тимати',
+        cover: 'assets/Images/cover 4.jpg'
+    },
+    {
+        name: 'Camry 3.5',
+        path: 'assets/Music/Slava Marlow - Camry 3.5.mp3',
+        artist: 'Slava Marlow',
+        cover: 'assets/Images/cover 5.jpg'
+    },
+    {
+        name: 'Снова Я Напиваюсь',
+        path: 'assets/Music/Slava Marlow - Снова Я Напиваюсь.mp3',
+        artist: 'Slava Marlow',
+        cover: 'assets/Images/cover 6.jpg'
+    },
+    {
+        name: 'Camry 3.5',
+        path: 'assets/Music/UncleFlexxx - Camry 3.5.mp3',
+        artist: 'UncleFlexxx',
+        cover: 'assets/Images/cover 7.jpg'
+    },
+    {
+        name: 'Монетка',
+        path: 'assets/Music/ЛСП - Монетка.mp3',
+        artist: 'ЛСП',
+        cover: 'assets/Images/cover 8.jpg'
+    },
+    {
+        name: 'Валим',
+        path: 'assets/Music/Нурминский - Валим.mp3',
+        artist: 'Нурминский',
+        cover: 'assets/Images/cover 9.jpg'
+    },
+    {
+        name: 'Мои Ясные Дни',
+        path: 'assets/Music/Олег Газманов - Мои Ясные Дни.mp3',
+        artist: 'Олег Газманов',
+        cover: 'assets/Images/cover 10.jpg'
+    },
+    {
+        name: 'Черный Бумер',
+        path: 'assets/Music/Серёга - Чёрный Бумер.mp3',
+        artist: 'Серёга',
+        cover: 'assets/Images/cover 11.jpg'
+    },
+    {
+        name: 'Cadillac',
+        path: 'assets/Music/Элджей & MORGENSHTERN - Cadillac.mp3',
+        artist: 'Элджей & MORGENSHTERN',
+        cover: 'assets/Images/cover 12.jpg'
+    }
+]
